3ebec6911b71c3952ae93f016897e0a2fd20e756
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-01-11 19:22:47 +0700

    update stuff

--- BusinessObjects/Entities/SampleContext.cs
+++ BusinessObjects/Entities/SampleContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace BusinessObjects.Entities;
 
@@ -21,8 +22,16 @@ public partial class SampleContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:ultraperformance.database.windows.net,1433;Initial Catalog=Sample;Persist Security Info=False;User ID=ultraperformance;Password=CvuLQbQtp4iKX2;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer(GetConnectionStrings());
 
+    private static string GetConnectionStrings()
+    {
+        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .Build();
+
+        return config.GetConnectionString("DefaultDB");
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Account>(entity =>
